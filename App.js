fc2079e7f31e02388d683b522f9c1be28ae391c2
unknown <zachariah11jerobert@gmail.com>
2022-02-21 07:39:23 +0530

    done stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { Button, Text, View } from "react-native";
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>This is a Home Screen</Text>
+      <Button
+        title="Go to Detail"
+        onPress={() => {
+          navigation.navigate("DetailScreen");
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailScreen({ navigation }) {
+  return (
+    <View>
+      <Text>This is a Detail Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => {
+          navigation.navigate("HomeScreen");
+        }}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
